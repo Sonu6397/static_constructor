59b2e1848719e5aae57e0c3ee2e4e5b43daaacb7
chhavi raj singh <chhavirajsingh2000@gmail.com>
2022-07-26 21:50:17 +0530

    Add project files.

---
+++ static_constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace staticConstractor
+{
+    public class employee
+    {
+        static employee()
+        {
+            Console.WriteLine("declear static constructir");
+        }
+    public static void Salary()
+    {
+        Console.WriteLine();
+        Console.WriteLine("The Salary method");
+    }
+}
+class details
+{
+    static void Main()
+    {
+      
+        Console.WriteLine();
+        employee.Salary();
+        Console.ReadLine();
+    }
+}
+}
